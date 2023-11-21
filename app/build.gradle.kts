3f88793a3fc872f9587457309eb62a4e82edba31
cashewcrewz <cherelisha.cruz.cics@ust.edu.ph>
2023-11-22 07:45:17 +0800

    Trying Gif and changed compile sdk

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.cashew"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.cashew"
@@ -38,10 +38,14 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
+    implementation("pl.droidsonroids.gif:android-gif-drawable:1.2.17") // implementation for GIF image
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+
+
 }
--- app/src/main/java/com/example/cashew/login_page.kt
+++ app/src/main/java/com/example/cashew/login_page.kt
@@ -1,11 +1,21 @@
 package com.example.cashew
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class login_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
+
+        val signupBtn : Button = findViewById(R.id.signupBtn)
+        val loginBtn = findViewById<Button>(R.id.loginBtn)
+
+        signupBtn.setOnClickListener{
+            val intent = Intent(this, signup_page::class.java)
+            startActivity(intent)
+        }
     }
 }
