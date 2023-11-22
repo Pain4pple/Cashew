288cecf03a8a0e451ecf569d74e15fdc2be3a0f0
LaraGatchalian <laranicole.gatchalian.cics@ust.edu.ph>
2023-11-22 12:04:07 +0800

    User Registration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
-    id("com.android.application")
 }
 
 android {
@@ -44,6 +43,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.firebase:firebase-database-ktx:20.3.0")
     testImplementation("junit:junit:4.13.2")
 
     implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
