9ddaaaafffe6f4534198861d92049ab496ff2c6a
cashewcrewz <cherelisha.cruz.cics@ust.edu.ph>
2023-11-22 09:35:27 +0800

    Update login_page.kt

--- app/src/main/java/com/example/cashew/login_page.kt
+++ app/src/main/java/com/example/cashew/login_page.kt
@@ -4,15 +4,21 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class login_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
 
-        //
+        // DECLARATIONS
         val signupBtn : Button = findViewById(R.id.signupBtn)
-        val loginBtn = findViewById<Button>(R.id.loginBtn)
+        val loginBtn : Button = findViewById(R.id.loginBtn)
+        var username : EditText = findViewById(R.id.usernameEditText)
+        var password : EditText = findViewById(R.id.passwordEditText)
+        
+
 
         loginBtn.setOnClickListener{
             val intent = Intent(this, orderways_page::class.java)
