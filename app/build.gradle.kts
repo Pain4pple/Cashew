55d6d754dfd7b21c7697bcab07157a5457637a1c
cashewcrewz <cherelisha.cruz.cics@ust.edu.ph>
2023-11-22 10:22:44 +0800

    firebase implementation for login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.gms.google-services")
-    id("com.android.application")
 }
 
 android {
@@ -45,12 +44,14 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
-
-    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
-    implementation("com.google.firebase:firebase-analytics")
-
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    // Import the BoM for the Firebase platform
+    implementation(platform("com.google.firebase:firebase-bom:32.6.0"))
+    // Add the dependency for the Firebase Authentication library
+    // When using the BoM, you don't specify versions in Firebase library dependencies
+    implementation("com.google.firebase:firebase-auth")
 
 
 
--- app/src/main/java/com/example/cashew/login_page.kt
+++ app/src/main/java/com/example/cashew/login_page.kt
@@ -6,18 +6,24 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
 
 class login_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_page)
 
+        private lateinit var auth: FirebaseAuth;
+        // ...
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
         // DECLARATIONS
         val signupBtn : Button = findViewById(R.id.signupBtn)
         val loginBtn : Button = findViewById(R.id.loginBtn)
         var username : EditText = findViewById(R.id.usernameEditText)
         var password : EditText = findViewById(R.id.passwordEditText)
-        
+
 
 
         loginBtn.setOnClickListener{
