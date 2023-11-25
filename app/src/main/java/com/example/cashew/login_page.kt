592e42041ed9fd677b048de19992e6abaf952408
cashewcrewz <cherelisha.cruz.cics@ust.edu.ph>
2023-11-25 17:37:45 +0800

    transparent status bar, username to name,

--- app/src/main/java/com/example/cashew/login_page.kt
+++ app/src/main/java/com/example/cashew/login_page.kt
@@ -3,8 +3,12 @@ package com.example.cashew
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.Window
+import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
+import androidx.appcompat.app.ActionBar
 
 
 class login_page : AppCompatActivity() {
@@ -13,7 +17,10 @@ class login_page : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
+        actionBar?.hide()
         setContentView(R.layout.activity_login_page)
+
+
 
 
 
@@ -32,6 +39,7 @@ class login_page : AppCompatActivity() {
             startActivity(intent)
         }
 
+        // SIGN-UP BUTTON TO REDIRECT TO SIGN UP PAGE
         signupBtn.setOnClickListener{
             val intent = Intent(this, signup_page::class.java)
             startActivity(intent)
