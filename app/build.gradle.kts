500fe1041e2f1c872945c1b43ea61beda5e91772
Painapple <105093357+Pain4pple@users.noreply.github.com>
2023-12-06 13:25:09 +0800

    updated products page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.firebase:firebase-database-ktx:20.3.0")
+    implementation ("com.google.android.material:material:1.0.0")
     implementation("com.google.firebase:firebase-auth:22.3.0")
     implementation("androidx.compose.ui:ui-graphics-android:1.5.4")
     testImplementation("junit:junit:4.13.2")
--- app/src/main/java/com/example/cashew/products_page.kt
+++ app/src/main/java/com/example/cashew/products_page.kt
@@ -1,17 +1,32 @@
 package com.example.cashew
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
-import com.example.cashew.models.user_model
-import com.example.cashew.objects.User
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cashew.models.product_model
+import com.example.cashew.objects.product_recycler
+
 
 class products_page : AppCompatActivity() {
-//var currentUser = User.currentUser
+    //var currentUser = User.currentUser
+    private lateinit var recyclerView : RecyclerView
+    private var productList : ArrayList<product_model> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products)
+        productList.add(product_model("1","Lemon Cake",R.drawable.lemon_cake,500))
+        productList.add(product_model("2","Pancake",R.drawable.pancake,600))
+        productList.add(product_model("3","Cinnamon Roll",R.drawable.cinnamonroll,200))
+        productList.add(product_model("4","Strawberry",R.drawable.strawberrysandwich,600))
+
+        recyclerView = findViewById(R.id.productsView)
+        recyclerView.setLayoutManager(GridLayoutManager(this, 2))
+        val productRecycler = product_recycler(productList)
+        recyclerView.adapter = productRecycler
 
         //DECLARATIONS
         val cashewBtn: ImageButton = findViewById(R.id.favoritebutton)
@@ -34,6 +49,8 @@ class products_page : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+
 
 
 }
