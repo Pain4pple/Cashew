9cc76d825b1d75857ec6bff95e623fbc603a9967
LaraGatchalian <laranicole.gatchalian.cics@ust.edu.ph>
2023-12-15 10:38:39 +0800

    changed ordermodel

--- app/src/main/java/com/example/cashew/models/order_model.kt
+++ app/src/main/java/com/example/cashew/models/order_model.kt
@@ -5,7 +5,7 @@ import java.util.Date
 data class order_model(var orderID:String?=null,
                        var userID: String?=null,
                        var orderProducts:ArrayList<cart_model>?=null,
-                       var orderDate: Date?=null,
+                       var orderDate: String?=null,
                        var totalAmount: Float? = 0f,
                        var modeOfPayment:String?=null,
                        var orderWay:String?=null)
